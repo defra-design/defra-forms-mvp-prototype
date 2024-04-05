09560a5444380c69bcbaea1a6099f1d5b4cbb21c
Colin Rotherham <work@colinr.com>
2024-04-05 08:19:21 +0100

    Add Nunjucks `require()` function

--- app/filters.js
+++ app/filters.js
@@ -1,10 +1,20 @@
+const { resolve } = require('path')
+const { cwd } = require('process')
+
 //
 // For guidance on how to create filters see:
 // https://prototype-kit.service.gov.uk/docs/filters
 //
 
 const govukPrototypeKit = require('govuk-prototype-kit')
-const addFilter = govukPrototypeKit.views.addFilter
+const { addFilter, addFunction } = govukPrototypeKit.views
 
 // Add your filters here
 
+addFunction('require', (path) => {
+  try {
+    return require(resolve(cwd(), 'app', path))
+  } catch {
+    return {}
+  }
+})

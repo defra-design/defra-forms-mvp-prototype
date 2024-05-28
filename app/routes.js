b961ee164db68eec1ef78ca3015d9791c9fcc103
daniel-defra <daniel.dasilveira@defra.gov.uk>
2024-05-28 12:09:43 +0100

    new kit of parts pages

--- app/routes.js
+++ app/routes.js
@@ -23,4 +23,44 @@ router.post('/duplicate-form-answer', function (req, res) {
       res.redirect('/new-edition/choose-a-form.html')
     }
 
-  })
+})
+
+router.post("/prototype-answer", function (request, response) {
+  var prototype = request.session.data["prototype"];
+  switch (prototype) {
+    case "editor":
+      response.redirect("/editor.html");
+      break;
+    case "overview":
+      response.redirect("/index-overview.html");
+      break;
+    case "library":
+      response.redirect("/library.html");
+      break;
+    case "signIn":
+      response.redirect(
+        "/form-designer-mvp2/onboarding/sign-in.html.html"
+      );
+      break;
+    default:
+      response.redirect("/ineligible-country");
+      break;
+  }
+});
+router.post("/overview-answer", function (request, response) {
+  var overviewValue = request.session.data["overview"];
+  switch (overviewValue) {
+    case "draft":
+      response.redirect("/cph-overview/draft.html");
+      break;
+    case "live":
+      response.redirect("/cph-overview/live-success.html");
+      break;
+    case "draftLive":
+      response.redirect("/cph-overview/live-draft-created.html");
+      break;
+    case "closed":
+      response.redirect("/cph-overview/closed-success.html");
+      break;
+  }
+});

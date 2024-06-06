54439441559a05d9f834cb1ad4bc0d9a6f7d16fd
daniel-defra <daniel.dasilveira@defra.gov.uk>
2024-06-06 16:22:35 +0100

    re-organised file structure

--- app/routes.js
+++ app/routes.js
@@ -3,33 +3,31 @@
 // https://prototype-kit.service.gov.uk/docs/create-routes
 //
 
-const govukPrototypeKit = require('govuk-prototype-kit')
-const router = govukPrototypeKit.requests.setupRouter()
+const govukPrototypeKit = require("govuk-prototype-kit");
+const router = govukPrototypeKit.requests.setupRouter();
 
 // Add your routes here
 
 // Run this code when a form is submitted to 'juggling-balls-answer'
-router.post('/duplicate-form-answer', function (req, res) {
+router.post("/duplicate-form-answer", function (req, res) {
+  // Make a variable and give it the value from 'how-many-balls'
+  var duplicateQuestion = req.session.data["duplicate-question"];
 
-    // Make a variable and give it the value from 'how-many-balls'
-    var duplicateQuestion = req.session.data['duplicate-question']
-
-    // Check whether the variable matches a condition
-    if (duplicateQuestion == "no"){
-      // Send user to next page
-      res.redirect('/name-of-form')
-    } else {
-      // Send user to ineligible page
-      res.redirect('/new-edition/choose-a-form.html')
-    }
-
-})
+  // Check whether the variable matches a condition
+  if (duplicateQuestion == "no") {
+    // Send user to next page
+    res.redirect("/name-of-form");
+  } else {
+    // Send user to ineligible page
+    res.redirect("/new-edition/choose-a-form.html");
+  }
+});
 
 router.post("/prototype-answer", function (request, response) {
   var prototype = request.session.data["prototype"];
   switch (prototype) {
     case "editor":
-      response.redirect("/editor0.html");
+      response.redirect("/editor/0.html");
       break;
     case "overview":
       response.redirect("/index-overview.html");
@@ -38,9 +36,7 @@ router.post("/prototype-answer", function (request, response) {
       response.redirect("/library.html");
       break;
     case "signIn":
-      response.redirect(
-        "/form-designer-mvp2/onboarding/sign-in.html.html"
-      );
+      response.redirect("/onboarding/sign-in.html.html");
       break;
     default:
       response.redirect("/ineligible-country");
@@ -51,16 +47,16 @@ router.post("/overview-answer", function (request, response) {
   var overviewValue = request.session.data["overview"];
   switch (overviewValue) {
     case "draft":
-      response.redirect("/cph-overview/draft.html");
+      response.redirect("/cph-overview/draft/complete-clean.html");
       break;
     case "live":
-      response.redirect("/cph-overview/live-success.html");
+      response.redirect("/cph-overview/live/01-clean.html");
       break;
     case "draftLive":
-      response.redirect("/cph-overview/live-draft-created.html");
+      response.redirect("/cph-overview/live+draft/01.html");
       break;
     case "closed":
-      response.redirect("/cph-overview/closed-success.html");
+      response.redirect("/cph-overview/closed/01-clean.html");
       break;
   }
 });

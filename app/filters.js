a8be631febda53991865dcabb7032c0e58179c59
Colin Rotherham <work@colinr.com>
2024-07-01 07:15:42 +0100

    Add advanced search filters

--- app/filters.js
+++ app/filters.js
@@ -1,5 +1,6 @@
 const { resolve } = require('path')
 const { cwd } = require('process')
+const { run } = require('node-jq')
 
 //
 // For guidance on how to create filters see:
@@ -18,3 +19,52 @@ addFunction('require', (path) => {
     return {}
   }
 })
+
+addFilter(
+  'search',
+
+  /**
+   * @param {FormLibrary['items']} items
+   * @param {Partial<Record<string, string | string[]>>} [data]
+   */
+  function (items, data = {}) {
+    const { author, name, organisation, status } = data
+
+    // Form name keyword search
+    if (name && typeof name === 'string') {
+      const keywords = name.toLowerCase().split(/[\s+]+/)
+
+      items = items.filter((item) =>
+        keywords.every((keyword) => item.name.toLowerCase().includes(keyword)),
+      )
+    }
+
+    // Created or updated by author search
+    if (Array.isArray(author) && author[0]) {
+      items = items.filter(
+        (item) =>
+          author.includes(item.created.name) ||
+          author.includes(item.updated.name)
+      )
+    }
+
+    // Organisation filter
+    if (Array.isArray(organisation) && organisation[0]) {
+      items = items.filter((item) => organisation.includes(item.organisation))
+    }
+
+    // Status filter
+    if (Array.isArray(status) && status[0]) {
+      items =
+        status.length === 1
+          ? items.filter((item) => status.includes(item.status))
+          : items.filter((item) => item.status === status.join('-'))
+    }
+
+    return items
+  }
+)
+
+/**
+ * @typedef {typeof import('./data/form-library.json')} FormLibrary
+ */

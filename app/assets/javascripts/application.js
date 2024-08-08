3e769a1332fd256fe9b390853478af78e97dd242
Colin Rotherham <work@colinr.com>
2024-08-08 10:11:09 +0100

    Remove duplicate script

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -43,7 +43,6 @@ class Example {
   }
 }
 
-export default Example;
 /**
  * The naming of things is a little complicated in here.
  * For reference:
@@ -276,50 +275,7 @@ class AppTabs {
   }
 }
 
-import iFrameResize from "iframe-resizer/js/iframeResizer.js";
 
-/**
- * Example component
- *
- * This allows automatic resizing of the iFrame pages contained within Example
- * template wrappers.
- *
- * @param {Element} $module - HTML element to use for example
- */
-class Example {
-  /**
-   * @param {Element} $module - HTML element
-   */
-  constructor($module) {
-    if (
-      !($module instanceof HTMLIFrameElement) ||
-      !document.body.classList.contains("govuk-frontend-supported")
-    ) {
-      return;
-    }
-
-    this.$module = $module;
-
-    // Initialise asap for eager iframes or browsers which don't support lazy loading
-    if (!("loading" in this.$module) || this.$module.loading !== "lazy") {
-      return iFrameResize({ scrolling: "omit" }, this.$module);
-    }
-
-    this.$module.addEventListener("load", () => {
-      try {
-        iFrameResize({ scrolling: "omit" }, this.$module);
-      } catch (error) {
-        if (error instanceof Error) {
-          console.error(error.message);
-        }
-      }
-    });
-  }
-}
-
-// export default Example
-
-// Initialise example frames
 const $examples = document.querySelectorAll(
   '[data-module="app-example-frame"]'
 );
